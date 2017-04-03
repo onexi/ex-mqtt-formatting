d06299670d1810e005523a41ecccfcede95da907
Tanguy Raguenez <tanguyra@mit.edu>
2017-04-03 11:12:13 -0400

    mqtt submission

--- tanguyraguenez/exercise.js
+++ tanguyraguenez/exercise.js
@@ -6,47 +6,28 @@ exercise.yourMessage = ""
 exercise.yourObject = ""
 
 exercise.ConnectToServer = function(address){
-    // Connect to the MQTT broker
-    // -------------------------------
-	// ---------- Your Code ----------
-	// -------------------------------
+    client = mqtt.connect(address, 1883);
 };
 
 exercise.SubscribeToChannel = function(channel){
-    // Subscribe to a channel
-    // -------------------------------
-	// ---------- Your Code ----------
-	// -------------------------------
+    client.subscribe(channel);
 };
 
 exercise.SendAsJSON = function(channel,obj){
-    // Publish an object to a channel
-    // as a JSON string
-    // -------------------------------
-	// ---------- Your Code ----------
-	// -------------------------------
+    client.publish(channel, JSON.stringify(obj));
 };
 
-exercise.ConvertBufferToString = function() {
-    // Convert a buffer used in some MQTT
-    // broker messages to a string
-    // -------------------------------
-	// ---------- Your Code ----------
-	// -------------------------------
+exercise.ConvertBufferToString = function(buffer) {
+    buffer.toString();
 };
 
 exercise.ProcessAsJSON = function() {
-    // Process an incoming message. as
-    // a JSON string.
-    // Set exercise.yourMessage to be
-    // the incoming message string, and
-    // set exercise.yourObject to be
-    // the parsed object.
-    // Also log the incoming message
-    // to console.
-    // -------------------------------
-	// ---------- Your Code ----------
-	// -------------------------------
+    client.on('message', function(topic,message){
+        var messageStr = message.toString();
+        console.log(messageStr);
+        console.log(topic);
+        exercise.yourObject = JSON.parse(messageStr);
+    });
 };
 
 ;
